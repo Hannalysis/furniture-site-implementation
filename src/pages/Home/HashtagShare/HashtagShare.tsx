c7952bc7ea09c0dd77cd04790c9ebcd2ff6cba5b
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2026-02-24 14:41:29 +0000

    refactor(styling): Removed classes for full-bleed containers.
    Repurposed grid for header and footer child containers.
    Added a bespoke wide class for banner container.

--- src/pages/Home/HashtagShare/HashtagShare.tsx
+++ src/pages/Home/HashtagShare/HashtagShare.tsx
@@ -1,12 +1,9 @@
 // import { Typography } from "../../components/@ui/Typography/Typography";
 import classes from "./HashtagShare.module.css"
-import layoutClasses from "../../../Layout/Layout.module.css"
 
 export const HashtagShare = () => (
-    <div className={layoutClasses.contentContainer}>
-        <div className={layoutClasses.grid}>
-            <section className={classes.hashtagShare}>
-            </section>
-        </div>
-    </div>
+    <section className={`
+        ${classes.hashtagShare}
+        `}>
+    </section>
 );
