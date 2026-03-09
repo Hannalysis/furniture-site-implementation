4f3fb1a69815f936e51a15625c2f8cb3f1e85367
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2026-03-09 14:02:54 +0000

    chore: Discover skeleton in with Typography linked & position
    Including discover padding, bg colour and content text

--- src/pages/Home/Banner/Banner.tsx
+++ src/pages/Home/Banner/Banner.tsx
@@ -1,4 +1,4 @@
-// import { Typography } from "../../components/@ui/Typography/Typography";
+import { Typography } from "../../../components/@ui/Typography/Typography";
 import classes from "./Banner.module.css"
 import layoutClasses from "../../../Layout/Layout.module.css"
 
@@ -11,9 +11,17 @@ export const Banner = () => (
         `}>
             <img
                 src={heroBackground}
-                alt="heroImage"
-                loading="lazy"
+                alt=""
                 className={classes.bannerImage}
             />
+            <div className={classes.bannerDiscoverBox}>
+                    <Typography variant="overline">New Arrival</Typography>
+                    <Typography variant="heading1">Discover Our New Collection</Typography>
+                    <Typography variant="subtitle1">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, 
+                        luctus nec ullamcorper mattis.
+                    </Typography>
+                    <button className={classes.bannerBuyButton}>BUY NOW</button>
+            </div>
     </section>
 );
