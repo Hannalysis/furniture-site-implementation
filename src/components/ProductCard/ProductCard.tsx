bbc22f3bdcbd33a8aadaf3f625adaa6e00777058
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2026-03-13 15:17:24 +0000

    feat: Implemented sticker component with class variants

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,22 +1,39 @@
 import { Typography } from "../@ui/Typography/Typography"
 import classes from "./ProductCard.module.css"
 
-type ProductCardData = {
+
+export type ProductCardData = {
   id: number
   title: string
   image: string
   desc: string
   price: string
   wasPrice?: string
+  sticker?: Sticker
 }
 
 type Props = {
   product: ProductCardData
 }
 
+export type Sticker = {
+  type: "sale" | "new" ;
+  label: string;                    
+};
+
+
 export function ProductCard({ product }: Props) {
   return (
     <div className={classes.productCard}>
+      {product.sticker && <div className={`
+      ${classes.productSticker} ${
+        product.sticker.type === "sale"
+        ? classes.sale
+        : product.sticker.type === "new"
+        ? classes.new
+        : ""
+      }`}>
+      <Typography variant="heading6">{product.sticker.label}</Typography></div>}
       <img src={product.image} alt={product.title} />
       <div className={classes.productContentContainer}>
         <Typography variant="heading4">{product.title}</Typography>
