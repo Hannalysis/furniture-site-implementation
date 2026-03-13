8970ae63d07969ec6da4c45f81320a2e6490f568
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2026-03-13 18:34:57 +0000

    refactor: Abstracted product data into bespoke location

--- src/pages/Home/OurProducts/OurProducts.tsx
+++ src/pages/Home/OurProducts/OurProducts.tsx
@@ -3,28 +3,10 @@ import classes from "./OurProducts.module.css"
 import layoutClasses from "../../../Layout/Layout.module.css"
 
 import { ProductCard } from "../../../components/ProductCard/ProductCard"
-import { type ProductCardData } from "../../../components/ProductCard/product.types"
 import { ShowMoreButton } from "../../../components/@ui/ShowMoreButton/ShowMoreButton";
 
-import imgProduct1 from "../../../assets/home/our-products/img-syltherine-stylish-cafe-table.jpg"
-import imgProduct2 from "../../../assets/home/our-products/img-leviosa-stylish-cafe-chair.jpg"
-import imgProduct3 from "../../../assets/home/our-products/img-lolito-luxury-big-sofa.jpg"
-import imgProduct4 from "../../../assets/home/our-products/img-respira-outdoor-bar-table-and-stool.jpg"
-import imgProduct5 from "../../../assets/home/our-products/img-grifo-night-lamp.jpg"
-import imgProduct6 from "../../../assets/home/our-products/img-muggo-small-mug.jpg"
-import imgProduct7 from "../../../assets/home/our-products/img-pingky-cute-bed-set.jpg"
-import imgProduct8 from "../../../assets/home/our-products/img-potty-minimalist-flower-pot.jpg"
+import { products } from "../../../data/products.data";
 
-const products: ProductCardData[]  = [
-  { id: 1, title: "Syltherine", image: imgProduct1, desc: "Stylish cafe table", price: "Rp 2.500.000", wasPrice: "Rp 3.500.000", sticker: {type: "sale", label: "-30%"}},
-  { id: 2, title: "Levisoa", image: imgProduct2, desc: "Stylish cafe chair", price: "Rp 2.500.000"},
-  { id: 3, title: "Lilito", image: imgProduct3, desc: "Luxury big sofa", price: "Rp 7.000.000", wasPrice: "Rp 14.000.000", sticker: {type: "sale", label: "-50%"}},
-  { id: 4, title: "Respira", image: imgProduct4, desc: "Outdoor bar table and stool", price: "Rp 500.000", sticker: {type: "new", label: "New"}},
-  { id: 5, title: "Grifo", image: imgProduct5, desc: "Night lamp", price: "Rp 1.500.000"},
-  { id: 6, title: "Muggo", image: imgProduct6, desc: "Small mug", price: "Rp 150.000", sticker: {type: "new", label: "New"}},
-  { id: 7, title: "Pingky", image: imgProduct7, desc: "Cute bed set", price: "Rp 7.000.000", wasPrice: "Rp 14.000.000", sticker: {type: "sale", label: "-50%"}},
-  { id: 8, title: "Potty", image: imgProduct8, desc: "Minimalist flower pot", price: "Rp 500.000", sticker: {type: "new", label: "New"}},
-]
 
 export const OurProducts = () => (
     <section className={`
