cfb942e7ff9c402a9d7b2bfde27af0ee268dba8a
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2026-03-16 15:42:28 +0000

    feat(styling): Added RoomCard content and button
    Bs react icon package reused for the right arrow icon.

--- src/pages/Home/Inspirations/Inspirations.tsx
+++ src/pages/Home/Inspirations/Inspirations.tsx
@@ -2,6 +2,11 @@ import { Typography } from "../../../components/@ui/Typography/Typography";
 import classes from "./Inspirations.module.css"
 import layoutClasses from "../../../Layout/Layout.module.css"
 
+import { BsArrowRight } from "react-icons/bs";
+// import { PiArrowRightThin } from "react-icons/pi";
+
+import imgInspirationsInnerPeace from "../../../assets/home/inspiration/img-inspiration-bed-room-inner-peace.jpg"
+
 
 export const Inspirations = () => (
     <section className={`${classes.inspirations}`}>
@@ -10,10 +15,29 @@ export const Inspirations = () => (
             ${classes.inspirationsContainer}`}>
                 <div className={classes.inspirationsHeaderContainer}>
                     <Typography variant="heading2">50+ Beautiful rooms inspiration</Typography>
-                    <Typography variant="heading6">Our designer already made a lot of beautiful prototipe of rooms that inspire you</Typography>
+                    <Typography variant="heading6">Our designer already made a lot of beautiful prototype of rooms that inspire you</Typography>
                     <button className={classes.buttonExploreMore}>Explore More</button>
                 </div>
-                <div className={classes.inspirationsRoomCardContainer}></div>
+                <div className={classes.inspirationsRoomCardContainer}>
+                    <img
+                        src={imgInspirationsInnerPeace}
+                        alt="Minimalist bed room with a wall of art in frames"
+                        className={classes.InspirationsImg}
+                    />
+                    <div className={classes.inspirationsRoomCardContentContainer}>
+                        <div className={classes.inspirationsRoomCardBox}>
+                            <div className={classes.inspirationsRoomCardLabel}>
+                                <Typography variant="heading6">01</Typography>
+                                <div className={classes.inspirationsLongEmDash}></div>
+                                <Typography variant="heading6">Bed Room</Typography>
+                            </div>
+                            <div className={classes.inspirationsInnerPeaceText}>Inner Peace</div>
+                        </div>
+                        <div className={classes.inspirationsButtonContainer}>
+                            <button className={classes.buttonRightArrow}><BsArrowRight className={classes.reactIcon} /></button>
+                        </div>
+                    </div>
+                </div>
                 <div className={classes.inspirationsCarousel}></div>
         </div>
     </section>
