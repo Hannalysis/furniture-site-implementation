5f3e5d9da8da6d4d6747d5c2cc80ced03628009c
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2026-03-16 13:27:43 +0000

    feat(styling): header container and contents added

--- src/pages/Home/Inspirations/Inspirations.tsx
+++ src/pages/Home/Inspirations/Inspirations.tsx
@@ -1,13 +1,20 @@
-// import { Typography } from "../../components/@ui/Typography/Typography";
+import { Typography } from "../../../components/@ui/Typography/Typography";
 import classes from "./Inspirations.module.css"
 import layoutClasses from "../../../Layout/Layout.module.css"
 
+
 export const Inspirations = () => (
     <section className={`${classes.inspirations}`}>
-        <div className= {
+        <div className={
             `${layoutClasses.contentContainer}
-            ${layoutClasses.grid}
             ${classes.inspirationsContainer}`}>
+                <div className={classes.inspirationsHeaderContainer}>
+                    <Typography variant="heading2">50+ Beautiful rooms inspiration</Typography>
+                    <Typography variant="heading6">Our designer already made a lot of beautiful prototipe of rooms that inspire you</Typography>
+                    <button className={classes.buttonExploreMore}>Explore More</button>
+                </div>
+                <div className={classes.inspirationsRoomCardContainer}></div>
+                <div className={classes.inspirationsCarousel}></div>
         </div>
     </section>
 );
