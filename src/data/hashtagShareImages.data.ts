f35c02b2bd76db00b1efb835b24b49149b83322b
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2026-03-20 14:41:08 +0000

    chore: Image data added and implemented

---
+++ src/data/hashtagShareImages.data.ts
@@ -0,0 +1,21 @@
+import imgShareTopLeft1 from "../assets/home/hashtag-share/img-share-top-left-1.jpg"
+import imgShareTopLeft2 from "../assets/home/hashtag-share/img-share-top-left-2.jpg"
+import imgShareBottomLeft1 from "../assets/home/hashtag-share/img-share-bottom-left-1.jpg"
+import imgShareBottomLeft2 from "../assets/home/hashtag-share/img-share-bottom-left-2.jpg"
+import imgShareCentre from "../assets/home/hashtag-share/img-share-centre.jpg"
+import imgShareTopRight1 from "../assets/home/hashtag-share/img-share-top-right-1.jpg"
+import imgShareTopRight2 from "../assets/home/hashtag-share/img-share-top-right-2.jpg"
+import imgShareBottomRight1 from "../assets/home/hashtag-share/img-share-bottom-right-1.jpg"
+import imgShareBottomRight2 from "../assets/home/hashtag-share/img-share-bottom-right-2.jpg"
+
+export const shareImages = [
+    imgShareTopLeft1,
+    imgShareTopLeft2,
+    imgShareBottomLeft1,
+    imgShareBottomLeft2,
+    imgShareCentre,
+    imgShareTopRight1,
+    imgShareTopRight2,
+    imgShareBottomRight1,
+    imgShareBottomRight2,
+];
--- src/pages/Home/HashtagShare/HashtagShare.tsx
+++ src/pages/Home/HashtagShare/HashtagShare.tsx
@@ -1,6 +1,9 @@
 import { Typography } from "../../../components/@ui/Typography/Typography";
 import classes from "./HashtagShare.module.css"
 
+import { shareImages } from "../../../data/hashtagshareImages.data"
+
+
 export const HashtagShare = () => (
     <section className={`
         ${classes.hashtagShare}
@@ -12,21 +15,52 @@ export const HashtagShare = () => (
         <div className={classes.shareImagesContainer}>
             <div className={classes.shareImagesLeftContainer}>
                 <div className={classes.shareImagesInnerTopLeftBox}>
-
+                    <img
+                        src={shareImages[0]}
+                        alt=""
+                    />
+                    <img
+                        src={shareImages[1]}
+                        alt=""
+                    />
                 </div>
                <div className={classes.shareImagesInnerBottomLeftBox}>
-
+                    <img
+                        src={shareImages[2]}
+                        alt=""
+                    />
+                    <img
+                        src={shareImages[3]}
+                        alt=""
+                    />
                 </div>
             </div>
             <div className={classes.shareImagesCentreContainer}>
-                
+                <img
+                    src={shareImages[4]}
+                    alt=""
+                />
             </div>
             <div className={classes.shareImagesRightContainer}>
                 <div className={classes.shareImagesInnerTopRightBox}>
-
+                    <img
+                        src={shareImages[5]}
+                        alt=""
+                    />
+                    <img
+                        src={shareImages[6]}
+                        alt=""
+                    />
                 </div>
                <div className={classes.shareImagesInnerBottomRightBox}>
-
+                    <img
+                        src={shareImages[7]}
+                        alt=""
+                    />
+                    <img
+                        src={shareImages[8]}
+                        alt=""
+                    />
                 </div>
             </div>
         </div>
