01036bfec1d40a536cf14390139a614cd3a4a384
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2026-03-31 13:59:25 +0100

    styling: Added smaller desktop responsive breakpoints
    3 columns, into 2 rows: A header w/ button, and
    the image & carousel as the second row.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import { Banner } from "./Banner/Banner"
 import { BrowseTheRange } from "./BrowseTheRange/BrowseTheRange";
 import { OurProducts } from "./OurProducts/OurProducts";
-// import { Inspirations } from "./Inspirations/Inspirations"
+import { Inspirations } from "./Inspirations/Inspirations"
 // import { HashtagShare } from "./HashtagShare/HashtagShare";
 
 export const Home = () => (
@@ -9,7 +9,7 @@ export const Home = () => (
     <Banner />
     <BrowseTheRange />
     <OurProducts />
-    {/* <Inspirations />
-    <HashtagShare /> */}
+    <Inspirations />
+    {/* <HashtagShare /> */}
   </>
 );
