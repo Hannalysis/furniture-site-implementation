519fee1c9b119adad3c0eea0f26231d09029f5c9
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2026-04-16 15:56:50 +0100

    debug: Container border & temp section removal
    To focus on each area of the landing page for ease of mobile responsive
    implementation.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,15 @@
-import { Banner } from "./Banner/Banner"
-import { BrowseTheRange } from "./BrowseTheRange/BrowseTheRange";
-import { OurProducts } from "./OurProducts/OurProducts";
-import { Inspirations } from "./Inspirations/Inspirations"
-import { HashtagShare } from "./HashtagShare/HashtagShare";
+// import { Banner } from "./Banner/Banner"
+// import { BrowseTheRange } from "./BrowseTheRange/BrowseTheRange";
+// import { OurProducts } from "./OurProducts/OurProducts";
+// import { Inspirations } from "./Inspirations/Inspirations"
+// import { HashtagShare } from "./HashtagShare/HashtagShare";
 
 export const Home = () => (
   <>
-    <Banner />
-    <BrowseTheRange />
-    <OurProducts />
-    <Inspirations />
-    <HashtagShare />
+    {/* <Banner /> */}
+    {/* <BrowseTheRange /> */}
+    {/* <OurProducts /> */}
+    {/* <Inspirations /> */}
+    {/* <HashtagShare /> */}
   </>
 );
